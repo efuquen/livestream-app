18f79e9bb5099eaa975061309f696e132de00241
Edwin Fuquen <efuquen@gmail.com>
2012-06-30 12:10:58 -0400

    added commons io, grabbing data from url, but not parsing json yet

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ scalacOptions in Compile ++= Seq(
 resolvers += "repo.codahale.com" at "http://repo.codahale.com"
 
 libraryDependencies ++= Seq(
-  "com.codahale" %% "jerkson" % "0.5.0"
+  "com.codahale" %% "jerkson" % "0.5.0",
+  "commons-io" % "commons-io" % "2.3"
 )
 
--- src/main/scala/com/livestream/events/EventActor.scala
+++ src/main/scala/com/livestream/events/EventActor.scala
@@ -2,13 +2,29 @@ package com.livestream.events
 
 import java.net.URL
 
+import java.io.StringWriter
+
+import org.apache.commons.io.IOUtils
+
 /**
  *  TODO: Not really an actor yet, but we'll deal with that later
 **/
 class EventActor {
 
+  private def getURLString(urlStr: String): String = {
+    val url = new URL(urlStr)
+    val urlIn = url.openStream
+    try {
+      val stringWriter = new StringWriter
+      IOUtils.copy(urlIn, stringWriter)
+      stringWriter.toString
+    } finally {
+      urlIn.close
+    }
+  }
+
   def isEventLive(event: Event): Boolean = {
-    val url = new URL(event.url)
+    val jsonStr = getURLString(event.url)
     false
   }
 }
